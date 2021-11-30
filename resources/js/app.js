29835e408d5b34a285bd0d44bd261d2ab4eb8e21
rezmehdi <rezaei_21@yahoo.com>
2021-11-30 11:04:49 +0330

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,14 @@
+
+import {createApp} from 'vue'
+
+require('./bootstrap')
+import App from './App.vue'
+import axios from 'axios'
+import router from './router'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const app = createApp(App)
+app.config.globalProperties.$axios = axios;
+app.use(router)
+app.mount('#app')
